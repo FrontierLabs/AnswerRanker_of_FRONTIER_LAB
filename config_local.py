2db6fe9b3afb1373286e7afe9c386e16428ff420
Jason <Jasonwbw@yahoo.com>
2019-01-29 14:02:40 +0800

    Update config_local.py

--- config_local.py
+++ config_local.py
@@ -1,17 +1,8 @@
 # -*- coding: utf-8 -*-
 
 machine = 'GPU'
-# machine = 'shareGPU'
-# machine = 'vm302'
-# machine = 'other'
 
 if machine == 'GPU':
 	base_model_folder = '/mnt/sdb/bowen/datas/model_context_lasagne/'
-elif machine == 'shareGPU':
-	base_model_folder = '/home/bowen/model/'
-elif machine == 'vm302':
-	base_model_folder = './model/'
-elif machine == 'other':
-	base_model_folder = './model/'
 else:
 	raise Exception('not support for machine ' + machine + ' now')
